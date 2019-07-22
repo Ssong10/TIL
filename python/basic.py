52d191b397e0ea6f1d4ffa09a9edd98d27902d0b
2seung10 <leesy1403@naver.com>
2019-07-22 14:31:30 +0900

    deleted

--- python/basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-key = 'cb8f5bac8d77d470b9b538c2106817b5'
-targetDt = '20190713'   #yyyymmdd
-api_url = f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key={key}&targetDt={targetDt}&weekGb=0'
-
-
-response = requests.get(api_url).json()
-print(response)
